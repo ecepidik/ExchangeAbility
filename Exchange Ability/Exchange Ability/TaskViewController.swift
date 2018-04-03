8919feccaebd4486ad0b5a6c61d5122d72f20f22
Courtney Wright <courtney.wright@mail.mcgill.ca>
2018-04-03 18:29:13 -0400

    Fix som ui stuff

--- Exchange Ability/Exchange Ability/TaskViewController.swift
+++ Exchange Ability/Exchange Ability/TaskViewController.swift
@@ -12,14 +12,35 @@ class TaskViewController:
 UIViewController {
 	//MARK: Properties
 	@IBOutlet weak var titleLabel: UILabel!
-
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var compensationLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleValue: UILabel!
+    @IBOutlet weak var categoryValue: UILabel!
+    @IBOutlet weak var dateValue: UILabel!
+    @IBOutlet weak var compensationValue: UILabel!
+    @IBOutlet weak var locationValue: UILabel!
+    @IBOutlet weak var descriptionValue: UILabel!
+    
+    
 	var task: Task?
 
 
 	override func viewDidLoad() {
         super.viewDidLoad()
 
-		titleLabel.text = task?.title
+        let dateFormatter = DateFormatter()
+        let date = dateFormatter.string(from: (task?.dateTime)!)
+        
+		titleValue.text = task?.title
+        categoryValue.text = task?.category.rawValue
+        dateValue.text = date
+        compensationValue.text = String(format: "%.2f", (task?.fee)!)
+        locationValue.text = task?.location
+        descriptionValue.text = task?.description
+        
         // Do any additional setup after loading the view.
     }
 
