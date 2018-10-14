cc5e03e153590731400088bf386528d00c1037aa
ecepidik <ece.pidik@mail.mcgill.ca>
2018-10-14 12:25:47 -0400

    added serverside

---
+++ ExchangeAbility-ServerSide/index.js
@@ -0,0 +1,45 @@
+/**
+ * Created by ece.pidik on 2018-10-12.
+ */
+
+const express = require('express')
+const app = express();
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+    user: 'exchangeability',
+    host: 'exchangeabilitydata.chwjpkpq8pgl.us-east-2.rds.amazonaws.com',
+    database: 'exchangeability',
+    password: 'exchangeability',
+    port: 5432,
+})
+
+const client = new Client({
+    user: 'exchangeability',
+    host: 'exchangeabilitydata.chwjpkpq8pgl.us-east-2.rds.amazonaws.com',
+    database: 'exchangeability',
+    password: 'exchangeability',
+    port: 5432,
+})
+client.connect()
+
+
+// app.listen(8000, () => {
+//     console.log('here');
+// });
+
+app.get('/', (req, res) => {
+    client.query('SELECT id, email, firstname, lastname, phone FROM public.users;', (err, obj) =>
+        {
+            res.send(obj.rows)})})
+
+
+
+app.listen(8000, () => console.log(`Example app listening on port 8000!`))
+
+// client.query('SELECT id, email, firstname, lastname, phone FROM public.users;', (err, res) => {
+//     app.listen(8000, () => {
+//     console.log('here');
+//     console.log(err, res)
+//     });
+// })
